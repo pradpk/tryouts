054163ebc21b19231dd837966d144f7a2adf06a7
pradpk <Pradhip.nit@gmail.com>
2019-04-25 14:00:20 -0400

    Create bubblesort.py

---
+++ python/bubblesort.py
@@ -0,0 +1,17 @@
+#Bubble sort is very slow (O(n^2)), but simplest of the sorting.
+#Rule
+#For below list, it will take elements from index 0 (7 from below) and place it in right position
+
+def bubble(arr):
+	for i in range(len(arr)-1, -1, -1):
+		print(arr)
+		for j in range(i+1):
+			if j+1<len(arr) and arr[j] > arr[j+1]:
+				tmp = arr[j+1]
+				arr[j+1] = arr[j]
+				arr[j] = tmp
+	print(arr)
+	
+	
+arr = [7,3,2,1,4]
+bubble(arr)
