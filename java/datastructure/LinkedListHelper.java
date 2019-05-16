238fa6a16ae13e1b4ed1df19793bd7621e91fc4a
pradpk <Pradhip.nit@gmail.com>
2019-05-16 10:49:01 -0400

    Create LinkedListHelper.java

---
+++ java/datastructure/LinkedListHelper.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+/**
+Class to apply LinkedList to use as queue and stack data structure.
+Queue is FIFO (First in First Out)
+Stack is LIFO (Last In First Out)
+*/
+public class LinkedListHelper {
+	
+	public static void main(String[] a) {
+		usePriorityQueue();
+	}
+	
+	private static void useAsQueue() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		for(int i=1;i<11;i++) {
+			list.offer(i);
+		}
+		
+		while(!list.isEmpty()) {
+			System.out.print(list.poll() + "\t");
+		}
+	}
+	
+	private static void useAsStack() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		for(int i=1;i<11;i++) {
+			list.offer(i);
+		}
+		
+		while(!list.isEmpty()) {
+			System.out.print(list.pollLast() + "\t");
+		}		
+	}
+	
+	
+	
+}
